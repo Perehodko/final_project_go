6caa44f66d5c2466f40765e5ef806743980c1371
nperehodko <nperekhodko@ozon.ru>
2025-09-17 15:19:45 +0300

    add changes

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = ``
+var Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwd2RfaGFzaCI6Ijk0YWVmYjhiZTc4YjJiN2MzNDRkMTFkMWJhOGE3OWVmMDg3ZWNlYjE5MTUwODgxZjY5NDYwYjg3NzI3NTMyNjMiLCJleHAiOjE3NTgxNDAxOTAsImlhdCI6MTc1ODExMTM5MH0.qhIqgtpgL_z2lIIethEmmfuZfKZKstru3bZi-KuynHo"
